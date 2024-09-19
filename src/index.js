160c43c7dd3af3e4bea0edd163647f8191d4d0be
Johan <johanotr@gmail.com>
2024-09-19 15:36:49 -0800

    Restart Logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { createToDo } from "./itemProcessor";
-import { AddToStorage } from "./storageHandler";
 import "./styles/JohansCSSReset.css";
 import "./styles/main.css";
-import  sidebar  from "./sidebar";
+
+
+
